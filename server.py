c5bb38b7db61bfce15b8cfaafded691a80337811
Rony Kati <ronykax@gmail.com>
2026-05-12 12:01:08 +0000

    move weights to gpu

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import tempfile
 
@@ -7,9 +8,10 @@ from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 asr_model = nemo_asr.models.ASRModel.from_pretrained(
-    model_name="nvidia/parakeet-tdt-0.6b-v3",
+    "nvidia/parakeet-tdt-0.6b-v3",
     map_location="cpu",  # type: ignore
 )
+asr_model = asr_model.cuda()  # type: ignore
 
 
 @app.post("/transcribe")
@@ -19,8 +21,9 @@ async def transcribe(file: UploadFile = File(...)):
         temp_path = temp_wav.name
 
     try:
-        output = asr_model.transcribe([temp_path])  # type: ignore
+        output = await asyncio.to_thread(asr_model.transcribe, [temp_path])  # type: ignore
+        text = output[0].text.rstrip() + " "
 
-        return {"text": output[0].text}
+        return {"text": text}
     finally:
         os.remove(temp_path)
